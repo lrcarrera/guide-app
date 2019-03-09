46c085f0354a7c08e4ed1be8f6c22dea20c0889a
Lluis <llmas1996@mail.com>
2019-03-09 19:34:29 +0100

    Postmodel

---
+++ app/src/main/java/com/example/pathfinderapp/Models/Post.java
@@ -0,0 +1,61 @@
+package com.example.pathfinderapp.Models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+    private Date createdAt;
+    private Date dueTo;
+    private User guide;
+    private List<User> tourists;
+    private float price;
+
+    public Post(Date createdAt, Date dueTo, User guide, List<User> tourists, float price) {
+        this.createdAt = createdAt;
+        this.dueTo = dueTo;
+        this.guide = guide;
+        this.tourists = tourists;
+        this.price = price;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDueTo() {
+        return dueTo;
+    }
+
+    public void setDueTo(Date dueTo) {
+        this.dueTo = dueTo;
+    }
+
+    public User getGuide() {
+        return guide;
+    }
+
+    public void setGuide(User guide) {
+        this.guide = guide;
+    }
+
+    public List<User> getTourists() {
+        return tourists;
+    }
+
+    public void setTourists(List<User> tourists) {
+        this.tourists = tourists;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
