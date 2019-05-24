e5b186bad98647b0ecbb410573ef8bd5b8b2a108
Lluis <16152037+LluisMas@users.noreply.github.com>
2019-05-24 21:39:37 +0200

    Notifications seems to work better now or Firebase is in a better mood. I don't know and I don't care

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ admin.initializeApp();
 
 exports.onPostCreat = functions.firestore
 .document('/posts/{postid}')
-.onWrite((change, context) => {
+.onWrite(async (change, context) => {
   console.log(`New post: ${context.params.postid}`)
 
   if (!change.after.exists) {
@@ -23,44 +23,29 @@ exports.onPostCreat = functions.firestore
       if (data.guide.uid !== null) {
         console.log('Uid: ', JSON.parse(JSON.stringify(data)));
 
+        const allUsers = await admin.firestore().collection('users').get();
+        allUsers.forEach(async function(value) {
+            const user = value.data().user;
+            if (user && user.uid == data.guide.uid) {
+              console.log('Query completed');
+              console.log('Data:', user);
+              const token = user.messageToken;
 
-        admin.firestore().collection('users').get().then(response => {
-          response.forEach(function(value) {
-              const user = value.data().user;
-              if (user && user.uid == data.guide.uid) {
-                console.log('Query completed');
-                console.log('Data:', user);
-                const token = user.messageToken;
+              console.log('TOKEN', token);
 
-                console.log('TOKEN', token);
-
-                const payload = {
-                  notification: {
-                    title: `New tourist!`,
-                    body: `You got a new tourist on your tour in ${data.place.name}`
-                  },
-                  token: token
-                }
-
-                admin.messaging().send(payload)
-                  .then( resp => {
-                    console.log('Successfully sent message:', resp);
-                  })
-                  .catch(function(error) {
-                    console.log('Error sending message:', error);
-                  });
-
-                // admin.messaging().sendToDevice(token, payload).then(function(resp) {
-                //     console.log('Successfully sent message:', resp);
-                //   })
-                //   .catch(function(error) {
-                //     console.log('Error sending message:', error);
-                //   });
+              const payload = {
+                notification: {
+                  title: `New tourist!`,
+                  body: `You got a new tourist on your tour in ${data.place.name}`
+                },
+                token: token
               }
-          })
-        })
-        .catch(err => console.log(err));
 
+              console.log('Messaging');
+              const resp = await admin.messaging().send(payload);
+              console.log('Done: ', resp);
+            }
+        })
       } else {
         console.log("Token is null", data.guide.uuid);
       }
